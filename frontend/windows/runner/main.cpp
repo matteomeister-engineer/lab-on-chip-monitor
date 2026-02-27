5740888450b5a14182f54d55d66cefff49cf9cfd
Mattéo Meister <matteomeister@matteos-macbook-pro.home>
2026-02-27 18:16:51 +0100

    Regenerate Windows runner, restore custom main.cpp

--- frontend/windows/runner/main.cpp
+++ frontend/windows/runner/main.cpp
@@ -1,3 +1,4 @@
+cat > windows/runner/main.cpp << 'EOF'
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
@@ -7,14 +8,10 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
@@ -41,3 +38,4 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoUninitialize();
   return EXIT_SUCCESS;
 }
+EOF
