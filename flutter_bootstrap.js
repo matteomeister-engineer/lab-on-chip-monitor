9a0af524827cf7566b2edb033ee7ded34454599e
matteomeister-engineer <matteomeister-engineer@users.noreply.github.com>
2026-03-06 14:22:16 +0000

    deploy: web app v1.5.0 a66bc7ffb2c4f75c9f0ae0100e3fdb86ea0ec14a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "461738814"
+    serviceWorkerVersion: "552519708"
   }
 });
